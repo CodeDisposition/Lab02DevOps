546a5b010339b99bdc7251ee1fed0756fb14aa66
CodeDisposition <aneebyuzai@hotmail.com>
2025-02-24 00:35:10 -0500

    Commit

---
+++ src/main/java/org/apache/maven/archetypes/Main.java
@@ -0,0 +1,18 @@
+package org.apache.maven.archetypes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/display")
+public class Main extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body><h1>Welcome to COMP367</h1></body></html>");
+    }
+}
